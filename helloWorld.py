d4189146f908094e4599996884bfbf6e63147f39
TheEBB <eve_cran@uri.edu>
2022-03-20 07:29:20 -0700

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,3 @@
-print ("Hello World, my name is Eve Cran and I love Python!")
+#print ("Hello World, my name is Eve Cran and I love Python!")
+userName = input ("Hello! Please enter your name.")
+print ("Hello,",userName,"! Nice to meet you.")
