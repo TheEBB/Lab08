967e3f6d496f6532278828d75078bf217b64d1eb
TheEBB <eve_cran@uri.edu>
2022-03-20 07:40:34 -0700

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,9 @@
 #print ("Hello World, my name is Eve Cran and I love Python!")
-userName = input ("Hello! Please enter your name.")
+
+userName = input ("Hello! Please enter your first name.")
 print ("Hello,",userName,"! Nice to meet you.")
+userPassword = input ("Please enter your password.")
+if(userName.lower() == "eve" and userPassword == "Passwordofmychoice"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
